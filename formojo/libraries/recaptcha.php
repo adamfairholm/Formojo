2f9dc1f79fcda349ea134ac7b2ca4d5f2c7c8ec0
Adam Fairholm <adam.fairholm@gmail.com>
2011-05-30 10:41:59 -0400

    Bypassing using a view for reCAPTCHA so it doesn't mess things up.

--- formojo/libraries/recaptcha.php
+++ formojo/libraries/recaptcha.php
@@ -164,14 +164,19 @@ class Recaptcha
     //load a view - more configurable than embedding HTML in the library
     
     // The rest of this function has been modified to work with MojoMotor/Formojo
-    
-    $orig_view_path = $this->_CI->load->_ci_view_path;
-
-	$this->_CI->load->_ci_view_path = APPPATH.'third_party/formojo/views/';
-    
-    return $this->_CI->load->view('recaptcha',$html_data,TRUE); 
-    
-    $this->_CI->load->_ci_view_path = $orig_view_path;
+        
+    return '<script type="text/javascript">
+  var RecaptchaOptions = { 
+    theme:"'.$html_data['theme'].'",
+    lang:"'.$html_data['lang'].'"
+  };
+</script>
+<script type="text/javascript" src="'.$html_data['server'].'/challenge?k='.$html_data['key'].$html_data['errorpart'].'"></script>
+<noscript>
+		<iframe src="'.$html_data['server'].'/noscript?lang=<?= $lang ?>&k='.$html_data['errorpart'].'" height="300" width="500" frameborder="0"></iframe><br/>
+		<textarea name="recaptcha_challenge_field" rows="3" cols="40"></textarea>
+		<input type="hidden" name="recaptcha" value="manual_challenge"/>
+</noscript>';    
   }
   
   /**
